3f8ce62543b107a9c0c50ab25a0a143b8adc2cb9
fasttrack611 <fasttrack611@gmail.com>
2025-06-09 22:41:45 +0530

    My First Sample CMake Project

---
+++ build/CMakeFiles/hello_app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_app.dir/main.cpp.obj"
+  "CMakeFiles/hello_app.dir/main.cpp.obj.d"
+  "CMakeFiles/hello_app.dir/src/hello.cpp.obj"
+  "CMakeFiles/hello_app.dir/src/hello.cpp.obj.d"
+  "hello_app.exe"
+  "hello_app.exe.manifest"
+  "hello_app.pdb"
+  "libhello_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/hello_app.dir/main.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/hello_app.dir/main.cpp.obj: C:\HelloCmakeApp\main.cpp \
+ C:/HelloCmakeApp/include/hello.h
